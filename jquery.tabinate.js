894de1cfcae2d52c075dfb8b594b3ad64354459c
Drew <drew@dangodesign.net>
2012-09-11 11:40:56 -0400

    Added ability to use same class selector for multiple tab instances. Probably not the most efficient way, but it works for what I need done today.

--- jquery.tabinate.js
+++ jquery.tabinate.js
@@ -1,4 +1,4 @@
-$.fn.tabinate = function(options) { // v0.4
+$.fn.tabinate = function(options) { // v0.5
 
 // Default Settings (don't adjust here!)
 	var settings = $.extend({
@@ -9,44 +9,47 @@ $.fn.tabinate = function(options) { // v0.4
 	}, options);
 
 	var container = this;
-	var tabs = container.find('.'+settings.tabClass);
+	container.each(function(i, e) {
+		var e = $(e);
+		var tabs = e.find('.'+settings.tabClass);
+		
+		// Initialize
+		tabs.removeClass(settings.activeTabClass).hide(0, function() {
+			var hash = window.location.hash;
+			hash = hash.slice(1);
 	
-	// Initialize
-	tabs.removeClass(settings.activeTabClass).hide(0, function() {
-		var hash = window.location.hash;
-		hash = hash.slice(1);
-
-		if($('#'+hash).length > 0) { // If hash, show tab
-			var eq = 0;
-			$('#'+hash+'.'+settings.tabClass).show(0, function() {
-				container.find("ul:eq(0) a[href='#"+hash+"']:eq(0)").addClass(settings.activeLinkClass);
-			});
-		}
-		if(container.find('.'+settings.tabClass+':visible').length == 0){ // If nothing visible, show first
-			var links = container.find('ul:eq(0) li:eq(0) a:eq(0)').addClass(settings.activeLinkClass);
-			tabs.eq(0).show().addClass(settings.activeTabClass);
-		}
-	});
-
-	// Tab Click
-	container.find('ul:eq(0) li a').click(function() {
-		container.find('ul:eq(0) a').not($(this)).removeClass(settings.activeLinkClass);
-		$(this).addClass(settings.activeLinkClass);
-		var targetdiv = $(this).attr('href');
-
-		if($(targetdiv).length > 0) { // if using ID href
-			if($(targetdiv).is(':hidden')) {
-				tabs.removeClass(settings.activeTabClass).hide(0, function() {
-					$(targetdiv).show().addClass(settings.activeTabClass);
+			if($('#'+hash).length > 0) { // If hash, show tab
+				var eq = 0;
+				$('#'+hash+'.'+settings.tabClass).show(0, function() {
+					e.find("ul:eq(0) a[href='#"+hash+"']:eq(0)").addClass(settings.activeLinkClass);
 				});
 			}
-		} else { // if not using IDs
-			eq = container.find('ul:eq(0) li a').index(this);
-			tabs.removeClass(settings.activeTabClass).hide(0, function() {
-				tabs.eq(eq).show().addClass(settings.activeTabClass);
-			});
-		}
-
-		if(settings.bookmarks != true) return false;
+			if(e.find('.'+settings.tabClass+':visible').length == 0){ // If nothing visible, show first
+				var links = e.find('ul:eq(0) li:eq(0) a:eq(0)').addClass(settings.activeLinkClass);
+				tabs.eq(0).show().addClass(settings.activeTabClass);
+			}
+		});
+	
+		// Tab Click
+		e.find('ul:eq(0) li a').click(function() {
+			e.find('ul:eq(0) a').not($(this)).removeClass(settings.activeLinkClass);
+			$(this).addClass(settings.activeLinkClass);
+			var targetdiv = $(this).attr('href');
+	
+			if($(targetdiv).length > 0) { // if using ID href
+				if($(targetdiv).is(':hidden')) {
+					tabs.removeClass(settings.activeTabClass).hide(0, function() {
+						$(targetdiv).show().addClass(settings.activeTabClass);
+					});
+				}
+			} else { // if not using IDs
+				eq = e.find('ul:eq(0) li a').index(this);
+				tabs.removeClass(settings.activeTabClass).hide(0, function() {
+					tabs.eq(eq).show().addClass(settings.activeTabClass);
+				});
+			}
+	
+			if(settings.bookmarks != true) return false;
+		});
 	});
 };
